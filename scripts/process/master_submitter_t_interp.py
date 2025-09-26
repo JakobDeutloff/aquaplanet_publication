9dbfa818b0d9a01062581c07186e1b0464c6524d
jakob.deutloff <jakob.deutloff@gmail.com>
2025-09-26 12:24:01 +0200

    deleted unnecessary files

--- scripts/process/master_submitter_t_interp.py
+++
@@ -1,7 +0,0 @@
-# %% 
-import os
-# %%
-runs = ["jed0011", "jed0022", "jed0033", "jed2224"]
-
-for run in runs:
-    os.system(f"sbatch scripts/process/submitter_t_interp.sh {run}")
--- scripts/process/regrid/master_submitter_regrid.py
+++
@@ -1,4 +0,0 @@
-import os 
-
-for run in ['jed0033']:
-    os.system(f"sbatch scripts/process/regrid/submit_regrid.sh {run}")
